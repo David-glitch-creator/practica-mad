f5f564d21c579a8fabe88c5c72f9f91d0bc0c0f6
David Sánchez Campos <david.sanchez.campos@udc.es>
2022-01-10 13:28:20 +0100

    Página de Logout

---
+++ PracticaMaD/Web/Pages/User/Logout.aspx.cs
@@ -0,0 +1,20 @@
+﻿using Es.Udc.DotNet.PracticaMaD.Web.HTTP.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.User
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SessionManager.Logout(Context);
+
+            Response.Redirect("~/Pages/MainPage.aspx");
+        }
+    }
+}
