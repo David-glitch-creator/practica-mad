66a6c4f7dffdd6be024d965f463c6ad1eddde9b8
David Sánchez Campos <david.sanchez.campos@udc.es>
2022-05-04 18:21:05 +0200

    Página Followed para ver la lista de usuarios seguidos (muy básica)

---
+++ PracticaMaD/Web/Pages/User/Followed.aspx.cs
@@ -0,0 +1,31 @@
+﻿using Es.Udc.DotNet.ModelUtil.IoC;
+using Es.Udc.DotNet.PracticaMaD.Model.UserService;
+using Es.Udc.DotNet.PracticaMaD.Web.HTTP.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.User
+{
+    public partial class Followed : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            IIoCManager ioCManager = (IIoCManager)Application["managerIoC"];
+
+            IUserService userService = ioCManager.Resolve<IUserService>();
+
+            UserInfo userInfo =
+                SessionManager.GetUserInfo(Context);
+
+            List<UserInfo> followed = userService.ViewFollowedUsers(userInfo.UserId);
+
+            Repeater1.DataSource = followed;
+            Repeater1.DataBind();
+        }
+    }
+}
