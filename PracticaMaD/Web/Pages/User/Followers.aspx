9609c45930cbaec3029d80b13407d67addbf6772
David Sánchez Campos <david.sanchez.campos@udc.es>
2022-05-09 16:47:28 +0200

    Página Followers para ver la lista de seguidores (muy básica también, a veces no funciona)

---
+++ PracticaMaD/Web/Pages/User/Followers.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Followers.aspx.cs" Inherits="Es.Udc.DotNet.PracticaMaD.Web.Pages.User.Followers" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Repeater ID="Repeater1" runat="server">
+                <ItemTemplate>
+                    <tr>
+                        <td><%# Eval("LoginName") %></td>
+                    </tr>
+                    <br />
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
+    </form>
+</body>
+</html>
---
+++ PracticaMaD/Web/Pages/User/Followers.aspx.cs
@@ -0,0 +1,30 @@
+﻿using Es.Udc.DotNet.ModelUtil.IoC;
+using Es.Udc.DotNet.PracticaMaD.Model.UserService;
+using Es.Udc.DotNet.PracticaMaD.Web.HTTP.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.User
+{
+    public partial class Followers : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IIoCManager ioCManager = (IIoCManager)Application["managerIoC"];
+
+            IUserService userService = ioCManager.Resolve<IUserService>();
+
+            UserInfo userInfo =
+                SessionManager.GetUserInfo(Context);
+
+            List<UserInfo> followed = userService.GetFollowers(userInfo.UserId);
+
+            Repeater1.DataSource = followed;
+            Repeater1.DataBind();
+        }
+    }
+}
