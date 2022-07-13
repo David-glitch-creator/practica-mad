910df510c9b2b614ebecde55ee6d8835117b5ea2
David Sánchez Campos <david.sanchez.campos@udc.es>
2022-07-13 12:38:03 +0200

    Añadida página de Actualizar perfil de usuario

---
+++ PracticaMaD/Web/Pages/User/UpdateUserProfile.aspx.cs
@@ -0,0 +1,95 @@
+﻿using Es.Udc.DotNet.PracticaMaD.Model.UserService;
+using Es.Udc.DotNet.PracticaMaD.Web.HTTP.Session;
+using Es.Udc.DotNet.PracticaMaD.Web.HTTP.View.ApplicationObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.User
+{
+    public partial class UpdateUserProfile : SpecificCulturePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                UserProfileDetails userProfileDetails =
+                    SessionManager.FindUserProfileDetails(Context);
+
+                txtFirstName.Text = userProfileDetails.FirstName;
+                txtSurname.Text = userProfileDetails.Lastname;
+                txtEmail.Text = userProfileDetails.Email;
+
+                /* Combo box initialization */
+                UpdateComboLanguage(userProfileDetails.Language);
+                UpdateComboCountry(userProfileDetails.Language,
+                    userProfileDetails.Country);
+            }
+        }
+
+        /// <summary>
+        /// Loads the languages in the comboBox in the *selectedLanguage*. 
+        /// Also, the selectedLanguage will appear selected in the 
+        /// ComboBox
+        /// </summary>
+        private void UpdateComboLanguage(String selectedLanguage)
+        {
+            this.comboLanguage.DataSource = Languages.GetLanguages(selectedLanguage);
+            this.comboLanguage.DataTextField = "text";
+            this.comboLanguage.DataValueField = "value";
+            this.comboLanguage.DataBind();
+            this.comboLanguage.SelectedValue = selectedLanguage;
+        }
+
+        /// <summary>
+        /// Loads the countries in the comboBox in the *selectedLanguage*. 
+        /// Also, the *selectedCountry* will appear selected in the 
+        /// ComboBox
+        /// </summary>
+        private void UpdateComboCountry(String selectedLanguage, String selectedCountry)
+        {
+            this.comboCountry.DataSource = Countries.GetCountries(selectedLanguage);
+            this.comboCountry.DataTextField = "text";
+            this.comboCountry.DataValueField = "value";
+            this.comboCountry.DataBind();
+            this.comboCountry.SelectedValue = selectedCountry;
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnUpdate control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance 
+        /// containing the event data.</param>
+        protected void BtnUpdateClick(object sender, EventArgs e)
+        {
+
+            if (Page.IsValid)
+            {
+                UserProfileDetails userProfileDetails =
+                    new UserProfileDetails(txtFirstName.Text, txtSurname.Text,
+                        txtEmail.Text, comboLanguage.SelectedValue,
+                        comboCountry.SelectedValue);
+
+                SessionManager.UpdateUserProfileDetails(Context,
+                    userProfileDetails);
+
+                Response.Redirect(
+                    Response.ApplyAppPathModifier("~/Pages/MainPage.aspx"));
+
+            }
+        }
+
+        protected void ComboLanguageSelectedIndexChanged(object sender, EventArgs e)
+        {
+            /* After a language change, the countries are printed in the
+             * correct language.
+             */
+            this.UpdateComboCountry(comboLanguage.SelectedValue,
+                comboCountry.SelectedValue);
+        }
+    }
+}
---
+++ PracticaMaD/Web/Pages/User/UpdateUserProfile.aspx.designer.cs
@@ -0,0 +1,179 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.User
+{
+
+
+    public partial class UpdateUserProfile
+    {
+
+        /// <summary>
+        /// Control lclMenuExplanation.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize lclMenuExplanation;
+
+        /// <summary>
+        /// Control UpdateUserProfileForm.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm UpdateUserProfileForm;
+
+        /// <summary>
+        /// Control lnkChangePassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink lnkChangePassword;
+
+        /// <summary>
+        /// Control lclFirstName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize lclFirstName;
+
+        /// <summary>
+        /// Control txtFirstName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFirstName;
+
+        /// <summary>
+        /// Control rfvFirstName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvFirstName;
+
+        /// <summary>
+        /// Control lclSurname.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize lclSurname;
+
+        /// <summary>
+        /// Control txtSurname.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSurname;
+
+        /// <summary>
+        /// Control rfvSurname.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvSurname;
+
+        /// <summary>
+        /// Control lclEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize lclEmail;
+
+        /// <summary>
+        /// Control txtEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
+        /// Control rfvEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvEmail;
+
+        /// <summary>
+        /// Control revEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator revEmail;
+
+        /// <summary>
+        /// Control lclLanguage.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize lclLanguage;
+
+        /// <summary>
+        /// Control comboLanguage.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList comboLanguage;
+
+        /// <summary>
+        /// Control lclCountry.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize lclCountry;
+
+        /// <summary>
+        /// Control comboCountry.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList comboCountry;
+
+        /// <summary>
+        /// Control btnUpdate.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnUpdate;
+    }
+}
